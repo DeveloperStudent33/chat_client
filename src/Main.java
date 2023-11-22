0b63a695680b94d34fa0213d272e199472756133
Niklas Müller <niklas.mueller@zdh.thm.de>
2023-11-22 14:35:40 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import de.thm.oop.chat.base.server.BasicTHMChatServer;
+
+public class Main {
+    public static void main(String[] args) {
+        BasicTHMChatServer server = new BasicTHMChatServer();
+        System.out.println();
+    }
+}
