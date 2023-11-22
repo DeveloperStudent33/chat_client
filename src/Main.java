6653b79f4fac9483a11a555129aa2e7eeeb01ab4
Niklas Müller <niklas.mueller@zdh.thm.de>
2023-11-22 14:59:03 +0100

    erste überlegungen zur Struktur + Test des Servers

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 import de.thm.oop.chat.base.server.BasicTHMChatServer;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BasicTHMChatServer server = new BasicTHMChatServer();
-        System.out.println();
+        String[] user = server.getUsers("Matschipapp", "qW8MVkr9");
+        for (String users: user) {
+            System.out.println(users);
+        }
     }
 }
